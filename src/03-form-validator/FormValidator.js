4e6ae542765cb1ee7deebf660d8869055c249ca6
sohupatel8828 <sohupatel8828@gmail.com>
2023-07-12 15:24:00 -0700

    Challenge 3 completed.

--- src/03-form-validator/FormValidator.js
+++ src/03-form-validator/FormValidator.js
@@ -5,8 +5,34 @@ export default function FormValidator () {
   const [password, setPassword] = useState('')
   const [passwordConfirm, setPasswordConfirm] = useState('')
 
+  const [validation, setValidation] = useState('')
+
+  const checkValidation = () => {
+    const validationError = []
+
+    if (!email || !password || !password){
+      validationError.push('All fields must be filled in')
+    }
+    if ((email.match(/@/g)||[]).length !== 1){
+      validationError.push('Email must have exactly one @ sign')
+    }
+    if (password.length < 8) {
+      validationError.push('Password must be 8 characters or longer')
+    }
+    if (password !== passwordConfirm){
+      validationError.push('Password and confirm password must be same')
+    }
+    return validationError;
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    const errors = checkValidation()
+    setValidation(errors.length ? errors.join(', ') : 'User created!');
+  } 
+  
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Sign Up!</h2>
       <label htmlFor='email'>Email</label>
       <input
@@ -23,6 +49,7 @@ export default function FormValidator () {
         type='password' name='password-confirm'
         onChange={e => setPasswordConfirm(e.target.value)}
       />
+      {validation}
       <input type='submit' value='Submit' />
     </form>
   )
